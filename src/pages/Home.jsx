7afaa585883638e967086a73c30204ece3c857af
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-07 16:32:59 +0300

    fix the auth error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <>
       <div>
-        <h2>If you have an account please sign in </h2>
-        <button onClick={() => navigate('/login')}>
+        <h2>hello </h2>
+        <button onClick={() => navigate('/signup')}>
           Click Here To Get Started
         </button>
       </div>
