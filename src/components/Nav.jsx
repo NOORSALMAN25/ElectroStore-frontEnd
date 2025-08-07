f604829d59d2665ae69fdefd3f0c332c9419adbc
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-07 16:06:05 +0300

    update nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,30 @@
 import { NavLink } from 'react-router-dom'
-const Nav = () => {
+import { Link } from 'react-router-dom'
+const Nav = ({ user, handleLogOut }) => {
+  let userOptions
+  if (user) {
+    userOptions = (
+      <>
+        <Link onClick={handleLogOut} to="/">
+          Sign Out
+        </Link>
+      </>
+    )
+  }
+
+  const publicOptions = (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/signup">Register</Link>
+      <Link to="/login">Login</Link>
+    </>
+  )
+
   return (
-    <nav className="navbar">
-      <NavLink to="/">
-        <div className="logo">🎇 Elecro store</div>
-      </NavLink>
-      <div className="nav-links">
-        <NavLink to="/">Home</NavLink>
-      </div>
-    </nav>
+    <header>
+      {user ? <h3>Welcome, {user.name}!</h3> : null}
+      <nav>{user ? userOptions : publicOptions}</nav>
+    </header>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react'
-import Login from '../components/Login'
-import SignUp from '../components/SignUp'
-import Nav from '../components/Nav'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
       <div>
         <h2>If you have an account please sign in </h2>
+        <button onClick={() => navigate('/login')}>
+          Click Here To Get Started
+        </button>
       </div>
     </>
   )
