ba273a8042c60b27db421e7123b326e01208ba48
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-08 20:49:25 +0300

    add the render in the productDetails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,6 +1,13 @@
 import ProductReview from './ProductReview'
 const ProductDetails = () => {
-  return <></>
+  return (
+    <>
+      <section>
+        <ProductReview productId={product.id} />
+        {/* the product should ref to product store in usestate */}
+      </section>
+    </>
+  )
 }
 
 export default ProductDetails
