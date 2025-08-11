e64f69d587e787050d41ec4a689c56527bafa622
mad6717 <mad6717@gmail.com>
2025-08-11 17:11:47 +0300

    commiting what is done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,17 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-const Cart = () => {}
+const Cart = () => {
+  const navigate = useNavigate()
+  const [order, setOrder] = useState(null)
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  return (
+    <>
+      <div className="cart-container"></div>
+    </>
+  )
+}
 
 export default Cart
