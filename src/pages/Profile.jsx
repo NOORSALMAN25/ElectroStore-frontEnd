65908cf953bc6cf167dd70c2ec2048677d2b993a
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-11 10:22:34 +0300

    set the useState and UseEffect and some functions

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,50 @@
+import { useEffect, useState } from 'react'
+import User from '../services/api'
+
 const Profile = ({ user }) => {
+  const [userr, setUserr] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [editing, setEditing] = useState(false)
+  const [showPasswordForm, setShowPasswordForm] = useState(false)
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [oldPassword, setOldPassword] = useState('')
+  const [NewPassword, setNewPassword] = useState('')
+  const [passwordMessage, setPasswordMessage] = useState('')
+
+  useEffect(() => {
+    console.log(user.id)
+
+    if (!user.id)
+      return User.get(`/profile/${user.id}`)
+        .then((res) => {
+          setUserr(res.data)
+          setName(res.data.name)
+          setEmail(res.data.email)
+          setLoading(false)
+        })
+        .catch((err) => {
+          setError(err.message)
+          setLoading(false)
+        })
+  }, [user.id])
+
+  const handleSave = () => {
+    User.put(`/profile/${user.id}`, { name, email })
+      .then((res) => {
+        setUserr(res.data)
+        setEditing(false)
+      })
+      .catch((err) => {
+        alert(`failed to update user information`)
+      })
+  }
+
+  if (loading) return <P>loading...</P>
+  if (error) return <p>error:{error}</p>
+
   return (
     <>
       <div>
